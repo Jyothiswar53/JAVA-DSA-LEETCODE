17b02397306c4ee6cc01e0a588bce48af32fa1bc
JYOTHISWAR <jyothiswar285@gmail.com>
2026-06-15 16:30:01 +0530

    Add files via upload

---
+++ HASHMAP/LongestConsecutiveSequence_128.java
@@ -0,0 +1,53 @@
+package HASHMAP;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class LongestConsecutiveSequence_128 {
+
+    public static int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        int longest = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i] - 1)) {
+                int currentNum = nums[i];
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                if (currentStreak > longest) {
+                    longest = currentStreak;
+                }
+            }
+        }
+
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Longest Consecutive Sequence Length: " + longestConsecutive(nums));
+
+        sc.close();
+    }
+}
